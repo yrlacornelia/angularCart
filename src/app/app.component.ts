fabe5398a4ed781e4df51354605154bf82e7a142
Yrla Hackzell <yrla.hackzell@hotmail.com>
2023-12-03 19:55:58 +0100

    components and api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NgModule } from '@angular/core';
+import { DataService } from './service/data.service';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet, HeaderComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularCart';
+  posts: Post[] = [];
+  errorMessage!: string;
+
+  constructor(private data_service: DataService) {}
+
+  ngOnInit() {
+    this.data_service.getAllPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+        console.log(this.posts);
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      },
+    });
+  }
 }
---
+++ src/app/service/data.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  apiUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+}
